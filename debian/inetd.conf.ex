2afe30d3a6802623de3faa918b052458f94d26b7
Kevin R. Thornton <krthornt@uci.edu>
2014-10-14 10:08:49 -0700

    deleted debian stuff

--- debian/inetd.conf.ex
+++
@@ -1,2 +0,0 @@
-#:OTHER:
-sequtils	stream	tcp	nowait	root	/usr/sbin/tcpd /usr/sbin/sequtils
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sequtils):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="sequtils" command="/usr/bin/sequtils"
